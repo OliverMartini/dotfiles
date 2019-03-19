c47480777be72f92417224850fc402aa24ccc2d2
Oliver Martini <oliver.martini@outlook.com>
2019-03-19 11:17:06 +0100

    Added WSL-Termain install

---
+++ wsl-terminal/install.ps1
@@ -0,0 +1,29 @@
+<#
+  .SYNOPSIS
+    Install WSL Terminal
+    
+  .NOTES
+    Author: Oliver Martini
+#>
+
+$toolDir = "C:\Tools"
+$version = "0.8.12"
+$wslTerminal = "wsl-terminal-$version.7z"
+
+Write-Progress -Activity "Get bits for WSL terminal"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://github.com/goreliu/wsl-terminal/releases/download/v$version/$wslTerminal" -OutFile $wslTerminal
+
+Write-Progress -Activity "Extract WSL terminal and remove after complete"
+Get-Item $wslTerminal | ForEach-Object {
+  $7z_Arguments = @(
+    'x'							## eXtract files with full paths
+    '-y'						## assume Yes on all queries
+    "`"-o$($toolDir)`""		## set Output directory
+    "`"$($_.FullName)`""				## <archive_name>
+  )
+  & 7z.exe $7z_Arguments
+  If ($LASTEXITCODE -eq 0) {
+    Remove-Item -Path $_.FullName -Force
+  }
+}

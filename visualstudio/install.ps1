afc9c26066d6c941ff3c377598abc876871e8aaa
Oliver Martini <oliver.martini@outlook.com>
2022-01-18 18:00:16 +0100

    Add VS2022

--- visualstudio/install.ps1
+++ visualstudio/install.ps1
@@ -6,28 +6,7 @@
     Author: Oliver Martini
 #>
 
-Write-Information "Install Visual Studio 2017" -InformationAction Continue
-#
-#--- Visual Studio 2017
-choco install -y visualstudio2017enterprise --package-parameters="'--add Microsoft.VisualStudio.Component.Git'" --limit-output
-choco install -y visualstudio2017-workload-universal        --limit-output
-choco install -y visualstudio2017-workload-manageddesktop   --limit-output
-choco install -y visualstudio2017-workload-netweb           --limit-output
-choco install -y visualstudio2017-workload-azure            --limit-output
-choco install -y visualstudio2017-workload-data             --limit-output
-choco install -y visualstudio2017-workload-netcrossplat     --limit-output
-choco install -y visualstudio2017-workload-nativecrossplat  --limit-output
-choco install -y visualstudio2017-workload-netcoretools     --limit-output
+Write-Information "Install Visual Studio 2022" -InformationAction Continue
+winget install Microsoft.VisualStudio.2022.Enterprise
 
-Write-Information "Install Visual Studio 2019" -InformationAction Continue
-#
-#--- Visual Studio 2019
-choco install -y visualstudio2019enterprise
-choco install -y visualstudio2019-workload-universal        --limit-output
-choco install -y visualstudio2019-workload-manageddesktop   --limit-output
-choco install -y visualstudio2019-workload-netweb           --limit-output
-choco install -y visualstudio2019-workload-azure            --limit-output
-choco install -y visualstudio2019-workload-data             --limit-output
-choco install -y visualstudio2019-workload-netcrossplat     --limit-output
-choco install -y visualstudio2019-workload-nativecrossplat  --limit-output
-choco install -y visualstudio2019-workload-netcoretools     --limit-output
+

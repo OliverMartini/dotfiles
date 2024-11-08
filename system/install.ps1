e2142b52a931b8f30e5877b8051ec6b88fe8c3db
Oliver Martini <oliver.martini@outlook.com>
2024-11-08 16:45:06 +0100

    added bitwarden

--- system/install.ps1
+++ system/install.ps1
@@ -14,4 +14,7 @@ winget install --silent sysinternals
 winget install --silent Microsoft.PowerToys
 winget install --silent OpenVPNTechnologies.OpenVPNConnect
 
+Write-Information "Install Bitwarden" -InformationAction Continue
+winget install --silent Bitwarden.Bitwarden
+
 RefreshEnv
